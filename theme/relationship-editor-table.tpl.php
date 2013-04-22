a76d5944b764218a0c34579441c6863874f85608
Rosemary Le Faive <lefaive@gmail.com>
2013-04-22 12:29:04 -0400

    Added Files

---
+++ theme/relationship-editor-table.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ *
+ * @TODO: needs documentation + list of all available variables
+ */
+?>
+<div id="relationship-form-main">
+  <div id="relationship-form-table">
+    <table>
+      <tr>
+        <th><?php print t('Relationship'); ?></th>
+        <th><?php print t('Target Object'); ?></th>
+        <th><?php print t('Delete'); ?></th>
+      </tr>
+      <?php foreach ($relationships as $relationship) : ?>
+      <tr>
+        <td><?php print $relationship['predicate'] ?></td>
+        <td><?php print $relationship['object'] ?></td>
+        <td><?php print $relationship['delete'] ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </table>
+  </div>
+</div>
