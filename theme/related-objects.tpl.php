6fea79d3c56fdb9d53130198187eaa59e4f22274
Rosie Le Faive <lefaive@gmail.com>
2015-04-09 16:43:45 -0300

    Rework hard-coded stuff for current project \n Exposing ways it should be configurable

--- theme/related-objects.tpl.php
+++ theme/related-objects.tpl.php
@@ -19,7 +19,7 @@
             <?php if(isset($obj['thumbnail_url'])): ?>
               <div class="thumb"><?php print $obj['thumbnail_url']; ?> </div>
             <?php endif; ?>
-            <div class="label"><?php print $obj['object_link']; ?></div>
+            <div class="label"><?php print $obj['relationship'] . ' : ' .  $obj['object_link']; ?></div>
           <?php endforeach; ?>
         </ul>
       </div>
